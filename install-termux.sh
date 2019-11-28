c63b76635351e00bb5452e66c972d469ad227540
MyAITech <53369303+MyAITech@users.noreply.github.com>
2019-11-28 17:07:59 +0400

    Update install-termux.sh

--- install-termux.sh
+++ install-termux.sh
@@ -1,38 +1,38 @@
 green='\033[1;32m'
 greens='\033[0;32m'
-echo "${green}update\n"
+echo -e "${green}update\n"
 pkg update -y
-echo "${green}installing ncftp${greens}\n"
+echo -e "${green}installing ncftp${greens}\n"
 pkg install ncftp -y
-echo "${green}installing php${greens}\n"
+echo -e "${green}installing php${greens}\n"
 pkg install php -y
-echo "${green}installing ssh${greens}\n"
+echo -e "${green}installing ssh${greens}\n"
 pkg install ssh -y
-echo "${green}installing curl${greens}\n"
+echo -e "${green}installing curl${greens}\n"
 pkg install curl -y
 echo "${green}installing sed${greens}\n"
 pkg install sed -y
-echo "${green}installing npm${greens}\n"
+echo -e "${green}installing npm${greens}\n"
 pkg install npm -y
-echo "${green}installing localtunnel${greens}\n"
+echo -e "${green}installing localtunnel${greens}\n"
 npm install -g localtunnel
-echo "${green}installing unzip${greens}\n"
+echo -e "${green}installing unzip${greens}\n"
 pkg install unzip -y
-echo "${green}installing wget${greens}\n"
+echo -e "${green}installing wget${greens}\n"
 pkg install wget -y
-echo "${green}making bin directory${greens}\n"
+echo -e "${green}making bin directory${greens}\n"
 mkdir bin
 echo "${green}opening bin directory${greens}\n"
 cd bin
-echo "${green}installing ngrok.zip${greens}\n"
+echo -e "${green}installing ngrok.zip${greens}\n"
 wget --output-document=ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
-echo "${green}unziping ngrok.zip${greens}\n"
+echo -e "${green}unziping ngrok.zip${greens}\n"
 unzip ngrok.zip
-echo "${green}removing ngrok.zip${greens}\n"
+echo -e "${green}removing ngrok.zip${greens}\n"
 rm ngrok.zip
-echo "${green}opening main directory${greens}\n"
+echo -e "${green}opening main directory${greens}\n"
 cd ../
-echo "${green}setting up permissions\n"
+echo -e "${green}setting up permissions\n"
 chmod -R 777 *
-echo "${green}autoremove${greens}\n"
-pkg autoremove -y
+echo -e "${green}autoremove${greens}\n"
+apt autoremove -y
