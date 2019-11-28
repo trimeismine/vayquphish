9987586c49866abd7c4791791f36d482be36a1b3
MyAITech <53369303+MyAITech@users.noreply.github.com>
2019-11-28 12:12:44 +0400

    ngrok support added for termux

--- install-termux.sh
+++ install-termux.sh
@@ -24,4 +24,15 @@ echo "${green}making bin directory${greens}\n"
 mkdir bin
 echo "${green}opening bin directory${greens}\n"
 cd bin
+echo "${green}installing ngrok.zip${greens}\n"
+wget --output-document=ngrok.zip https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm64.zip
+echo "${green}unziping ngrok.zip${greens}\n"
+unzip ngrok.zip
+echo "${green}removing ngrok.zip${greens}\n"
+rm ngrok.zip
+echo "${green}opening main directory${greens}\n"
+cd ../
+echo "${green}setting up permissions\n"
+chmod -R 777 *
+echo "${green}autoremove${greens}\n"
 pkg autoremove -y
